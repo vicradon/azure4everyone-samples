425896c58f0e6cb58e00ad30c4fe60bfb8b21a2e
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2024-07-27 11:46:11 +0100

    chore: modify script to output echos

--- azure-load-balancer-introduction/environment-create.sh
+++ azure-load-balancer-introduction/environment-create.sh
@@ -6,6 +6,7 @@ vnetname=vm-vnet
 
 # Create the resource group
 az group create -g $group -l $location
+echo "Created resource group"
 
 # Create Virtual Network and Subnet
 az network vnet create \
@@ -15,23 +16,26 @@ az network vnet create \
   --address-prefixes '192.168.0.0/16' \
   --subnet-name subnet \
   --subnet-prefixes '192.168.1.0/24'
+echo "Created virtual network"
 
 # Create Availability Set
 az vm availability-set create \
   -n vm-as \
   -l $location \
   -g $group
+echo "Created availability set"
 
 # Create Network Security Group (NSG)
 az network nsg create \
   -g $group \
   -n vm-nsg
+echo "Created network security group"
 
 # Create VMs in a loop
 for NUM in 1 2 3
 do
   az vm create \
-    -n vm-eu-0$NUM \
+    -n vm-0$NUM \
     -g $group \
     -l $location \
     --size Standard_B1s \
@@ -44,12 +48,15 @@ do
     --availability-set vm-as \
     --nsg vm-nsg
 done
+echo "Created virtual machines"
 
 # Open port 80 for each VM
 for NUM in 1 2 3
 do
-  az vm open-port -g $group --name vm-eu-0$NUM --port 80
+  az vm open-port -g $group --name vm-0$NUM --port 80
 done
+echo "Opened port 80 in VMs"
+
 
 # Install IIS on each VM using Custom Script Extension
 for NUM in 1 2 3
@@ -60,5 +67,6 @@ do
     -g $group \
     --publisher Microsoft.Compute \
     --version 1.8 \
-    --settings '{"commandToExecute":"powershell Add-WindowsFeature Web-Server; powershell Add-Content -Path \\"C:\\inetpub\\wwwroot\\Default.htm\\" -Value \\"$($env:computername)\\""}'
+    --settings "{\"commandToExecute\":\"powershell -Command \\\"Add-WindowsFeature Web-Server; Add-Content -Path 'C:\\inetpub\\wwwroot\\Default.htm' -Value '<h1>Server $NUM</h1>'\\\"\"}"
 done
+echo "Setup IIS and set the content of the default web servers"
